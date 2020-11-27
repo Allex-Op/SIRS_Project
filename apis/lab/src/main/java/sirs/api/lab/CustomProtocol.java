b49df98227c048589ee532bf9c7950e95fb27141
rafaelatimoteo <rafaelatimoteo@live.com.pt>
2020-11-27 15:50:03 +0000

    added extractPubKey and encrypt functions to custom protocol

--- apis/lab/src/main/java/sirs/api/lab/CustomProtocol.java
+++ apis/lab/src/main/java/sirs/api/lab/CustomProtocol.java
@@ -3,15 +3,48 @@ package sirs.api.lab;
 import sirs.api.lab.entities.CustomProtocolResponse;
 import sirs.api.lab.entities.TestResponse;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.*;
+import java.nio.file.CopyOption;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import static java.nio.file.Files.readAllBytes;
+
 //TODO: This is just a possible sketch
 public class CustomProtocol {
     String sessionKey;  //TODO: Esta session key devem receber do hospital ou gerar aqui, decidam
                         // como acharem melhor... se receberem do hospital talvez adicionar um endpoint
                         // especifico para isso?
 
-    public String encryptData(TestResponse plaintextData) {
-        //TODO: Use the generated session key to encrypt the data adding Confidentiality,Integrity & Freshness
-        String cipheredText = plaintextData.getResults() + plaintextData.getDigitalSignature();
+    public PublicKey extractPubKey(String certificate) throws CertificateException {
+
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream cert = new ByteArrayInputStream(certificate.getBytes());
+        Certificate final_certificate = cf.generateCertificate(cert);
+        PublicKey pubKey = final_certificate.getPublicKey();
+
+        return pubKey;
+    }
+
+    public byte[] encryptData(byte[] randomString, PublicKey pubKey) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, IOException, BadPaddingException, IllegalBlockSizeException {
+
+        //TODO: Encrypt the data adding Confidentiality, Integrity & Freshness
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+        byte[] cipheredText = cipher.doFinal(randomString);
+
         return cipheredText;
     }
 
--- apis/lab/src/main/java/sirs/api/lab/Handlers.java
+++ apis/lab/src/main/java/sirs/api/lab/Handlers.java
@@ -9,7 +9,24 @@ import sirs.api.lab.entities.CustomProtocolResponse;
 import sirs.api.lab.entities.TestRequest;
 import sirs.api.lab.entities.TestResponse;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.IOException;
 import java.net.http.HttpClient;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Random;
+
+import static java.nio.file.Files.readAllBytes;
 
 @RestController
 public class Handlers {
@@ -17,7 +34,7 @@ public class Handlers {
     CustomProtocol cp = new CustomProtocol();
 
     @PostMapping("/teststoanalyze/{id}")
-    public ResponseEntity<TestResponse> testsToAnalyze(@PathVariable int id, @RequestBody TestRequest testreq) {
+    public ResponseEntity<TestResponse> testsToAnalyze(@PathVariable int id, @RequestBody TestRequest testreq) throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, CertificateException {
         // Because for simplicity reasons we only answer to requests with id 1
         // id is only for representation purposes in case this was a real system
         // we would have multiple id's...
@@ -30,8 +47,19 @@ public class Handlers {
         System.out.println(data);
         System.out.println(certificate);
 
-        //TODO: verify certificate -> generate random string -> encrypt it with the public key of the Lab -> send
+        //TODO: verify certificate -> generate random string (yes) -> encrypt it with the public key of the Lab (yes) -> send
 
+        // Generating random string
+        byte[] randomString = new byte[64];
+        new Random().nextBytes(randomString);
+
+        // Extract pub key from certificate
+        PublicKey pubKey = cp.extractPubKey(certificate);
+
+        // Encrypt random string with pub key
+        byte[] encrypted_data = cp.encryptData(randomString, pubKey);
+
+        //TODO: send encrypted random string
         String results = "25/05/2020 Covid19:True,Pneumonia:True...";
         String signature = cr.signData(results);
         TestResponse resp = new TestResponse(results, signature);
@@ -39,8 +67,6 @@ public class Handlers {
 //        if(signature.equals(""))
 //            return ResponseEntity.status(500).build();
 
-        //TODO: ADD CUSTOM SECURITY CHANNEL HERE
-        String safeData = cp.encryptData(resp);
 
         return ResponseEntity.ok(resp);
     }
