b95978fce44d889364f3a0ea196035e852bb4dbf
amsce-iscteiul <Ana_Marta_Contente@iscte-iul.pt>
2020-11-28 14:09:11 +0000

    Update Handlers.java
    correct path

--- apis/hospital/src/main/java/sirs/api/hospital/Handlers.java
+++ apis/hospital/src/main/java/sirs/api/hospital/Handlers.java
@@ -108,10 +108,11 @@ public class Handlers {
     public ResponseEntity<TestResponse> sendTestToLab(@PathVariable int id) {
         try {
             //TODO: ADD CUSTOM SECURITY CHANNEL HERE
-
+            System.out.println("entered");
             // Getting the certificate to send along with the data in TestRequest
-            File crtFile = new File("/home/rafaela/Desktop/SIRS/Project/SIRS_Project/apis/hospital/src/main/resources/hospital.pem");
+            File crtFile = new File("src/main/resources/hospital.pem");
             String certificate = new String(Files.readAllBytes(crtFile.toPath()), Charset.defaultCharset());
+            System.out.println("after fule");
 
             TestRequest req = new TestRequest("RANDOM STUFF THIS DOESNT MATTER IS JUST TO SIMULATE A REQUEST", certificate);
 
