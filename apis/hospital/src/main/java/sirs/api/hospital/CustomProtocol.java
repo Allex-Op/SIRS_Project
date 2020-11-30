37481fb984f8e735aa3ba75119f3f1abdba48a0a
rafaelatimoteo <rafaelatimoteo@live.com.pt>
2020-11-30 19:26:48 +0000

    minor changes

--- apis/hospital/src/main/java/sirs/api/hospital/CustomProtocol.java
+++ apis/hospital/src/main/java/sirs/api/hospital/CustomProtocol.java
@@ -20,24 +20,6 @@ import java.util.*;
 public class CustomProtocol {
     String sessionKey;
 
-    public boolean initHandshake() {
-        //TODO: Change certificates with HTTP requests
-        // Verify if the certificate belongs to the LAB (important)
-        // Generate random string and encrypt it with the HOSPITAL public key
-        // Generate sessionKey here or on the lab & store it for further use...
-        sessionKey = "superSecretKey";  // The type of the key won't be string btw, this is just an example
-        return true;
-    }
-
-    public byte[] encryptData(byte[] randomString, PublicKey pubKey) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, IOException, BadPaddingException, IllegalBlockSizeException {
-        // Encrypt the data adding Confidentiality, Integrity & Freshness
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-        byte[] cipheredText = cipher.doFinal(randomString);
-
-        return cipheredText;
-    }
-
     public byte[] decryptData(byte[] cipheredData, PrivateKey privKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         // Decrypt the data, verify integrity and freshness
         Cipher decrypt=Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
@@ -90,29 +72,6 @@ public class CustomProtocol {
         in.close();
 
         return resp;
-    }
-
-    public boolean verifyCertificate(Certificate certToCheck, String trustedAnchor) throws FileNotFoundException, CertificateException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
-
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        List mylist = new ArrayList();
-        mylist.add(certToCheck);
-
-        CertPath cp = cf.generateCertPath(mylist);
-        FileInputStream in = new FileInputStream(trustedAnchor);
-        Certificate trust = cf.generateCertificate(in);
-        TrustAnchor anchor = new TrustAnchor((X509Certificate) trust, null);
-        PKIXParameters params = new PKIXParameters(Collections.singleton(anchor));
-        params.setRevocationEnabled(false);
-        CertPathValidator cpv = CertPathValidator.getInstance("PKIX");
-        PKIXCertPathValidatorResult result = null;
-        try {
-            result = (PKIXCertPathValidatorResult) cpv.validate(cp, params);
-            return true;
-        } catch (CertPathValidatorException e) {
-            System.out.println("Invalid Certificate");
-            return false;
-        }
     }
 
     public boolean verifyIntegrity(String data) {
