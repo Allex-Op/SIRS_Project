f245322d9fa88c9dc99b202684977531dfd37714
amsce-iscteiul <Ana_Marta_Contente@iscte-iul.pt>
2020-12-01 14:25:04 +0000

    Update Crypto.java
    static

--- apis/lab/src/main/java/sirs/api/lab/Crypto.java
+++ apis/lab/src/main/java/sirs/api/lab/Crypto.java
@@ -3,19 +3,19 @@ package sirs.api.lab;
 import java.security.Signature;
 
 public class Crypto {
-    private String algo = "SHA256WithRSA";
+    private static final String SIGNATURE = "SHA256WithRSA";
 
     /**
      *  Used to digitally sign the test results,
      *  this function result is sent in the response when an hospital
      *  requests for test results data.
      */
-    public String signData(String data) {
+    public static String signData(String data) {
         try {
             //TODO: Read the private key from a keyStore, for that you first have to find out
             //TODO: how to add key to privateStore...
             //TODO: (ps important: use the algorithm specified in the algo variable)
-            Signature signature = Signature.getInstance(algo);
+            Signature signature = Signature.getInstance(SIGNATURE);
             return "";
         } catch(Exception e) {
             System.out.println("Something went wrong while signing the data...");
